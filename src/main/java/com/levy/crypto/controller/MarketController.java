d9eb3b79376de363f19c64457cc57f5b4f7c07fe
Le Vy Cao <levycaoesc14@gmail.com>
2026-05-28 20:12:13 +1000

    add 3 api endpoints, get top performers, bottom performers, and the coin itself

--- src/main/java/com/levy/crypto/controller/MarketController.java
+++ src/main/java/com/levy/crypto/controller/MarketController.java
@@ -3,17 +3,29 @@ package com.levy.crypto.controller;
 import com.levy.crypto.model.MarketTicker;
 import com.levy.crypto.service.MarketService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class MarketController {
 
     private final MarketService marketService;
     public MarketController(MarketService marketService){
         this.marketService = marketService;
     }
-//    @GetMapping("/top-performers")
-//    public List<MarketTicker> getTopPerformers() {
-//        return marketService.getTopPerformers();
-//    }
+    @GetMapping("/top-performers")
+    public List<MarketTicker> getTopPerformers() {
+        return marketService.getTopPerformers();
+    }
+    @GetMapping("/bottom-performers")
+    public List<MarketTicker> getBottomPerformers() {
+        return marketService.getBottomPerformers();
+    }
+    @GetMapping("/coin/{symbol}")
+    public MarketTicker getCoin(@PathVariable String symbol){
+        return marketService.getCoin(symbol);
+    }
 }
--- src/main/java/com/levy/crypto/service/MarketService.java
+++ src/main/java/com/levy/crypto/service/MarketService.java
@@ -1,29 +1,62 @@
 package com.levy.crypto.service;
 
 import com.levy.crypto.model.MarketTicker;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
+@Getter
+@Setter
 public class MarketService {
     private final BinanceService binanceService;
     private List<MarketTicker> latestData = new ArrayList<>();
-    private List<MarketTicker> historyData = new ArrayList<>();
+    private Map<String, List<MarketTicker>> historyData = new HashMap<>();
     public MarketService(BinanceService binanceService){
         this.binanceService = binanceService;
     }
     @Scheduled(fixedRate = 5000)
-    public List<MarketTicker> fetchPrices() {
+    public void fetchPrices() {
        List<MarketTicker> marketTickerList =  binanceService.fetchData();
-       historyData.addAll(marketTickerList);
        latestData = marketTickerList.stream()
                 .filter(ticker -> ticker.getSymbol().endsWith("USDT"))
-                .sorted((a, b) -> Double.compare(b.getChangePercent(), a.getChangePercent()))
+                .sorted(Comparator.comparing(MarketTicker::getChangePercent).reversed())
                 .toList();
-       return marketTickerList;
+       storeHistory(latestData);
+    }
+
+    private void storeHistory(List<MarketTicker> data) {
+        for (MarketTicker marketTicker : data) {
+            String symbol = marketTicker.getSymbol();
+
+            historyData.putIfAbsent(symbol, new ArrayList<>());
+            historyData.get(symbol).add(marketTicker);
+
+            if (historyData.get(symbol).size() > 100) {
+                historyData.get(symbol).remove(0);
+            }
+        }
+    }
+
+    public List<MarketTicker> getTopPerformers() {
+        return latestData.stream().limit(10).toList();
+    }
+    public List<MarketTicker> getBottomPerformers() {
+        return latestData.stream()
+                .skip(Math.max(0, latestData.size() - 10))
+                .toList();
+    }
+
+    public MarketTicker getCoin(String symbol) {
+        String normalizedSymbol = symbol.toUpperCase();
+
+        return latestData.stream()
+                .filter(data -> data.getSymbol().equals(normalizedSymbol))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Coin not found"));
     }
 }
